c6044c1b96ae598939bfcfd9ca27ab2153bcb4b0
Frédéric KOLLER <43694853+fredckl@users.noreply.github.com>
2021-04-03 08:47:59 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <h1>ToDo list</h1>
+      <add-todo @onAddTodo="handlAddTodo"/>
+      <list-todo :todos="todos"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddTodo from './components/AddTodo';
+import ListTodo from './components/ListTodo.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddTodo,
+    ListTodo
+  },
+  data () {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    handlAddTodo (todo) {
+      this.$set(this, 'todos', [...this.todos, todo])
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>

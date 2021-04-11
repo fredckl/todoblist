bd5742d1ed757460a0cb324e9f232216a95aa902
Frédéric KOLLER <43694853+fredckl@users.noreply.github.com>
2021-04-11 09:46:26 +0200

    passe props to todo count

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,21 +1,22 @@
 <template>
   <div class="list-todo">
-    <todos-count/>
+    <todos-count :todos="todos"/>
     <todo-filter
       @showOpen="filterMode = 'showOpen'"
       @showClosed="filterMode = 'showClosed'"
       @showAll="filterMode = 'showAll'"/>
+    <todo-search @onSubmit="onSearch"/>
     <ul v-if="todos.length">
       <todo-list-item
-      v-for="({id, label, createdAt, description , archived}) in todos"
-      :key="id"
-      :label="label"
-      :id="id"
-      :createdAt="createdAt"
-      :archived='archived'
-      :description="description"
-      @onRemove="handleRemoveItem"
-    />
+        v-for="({id, label, createdAt, description , archived}) in todos"
+        :key="id"
+        :label="label"
+        :id="id"
+        :createdAt="createdAt"
+        :archived='archived'
+        :description="description"
+        @onRemove="handleRemoveItem"
+      />
     </ul>
     <confirm-dialogue
       ref="confirmDialogue"
@@ -31,37 +32,63 @@ import TodoListItem from './TodoListItem';
 import ConfirmDialogue from './ConfirmDialogue';
 import TodosCount from './TodosCount';
 import TodoFilter from './TodoFilter.vue';
+import TodoSearch from './TodoSearch.vue';
+import { compose, prop, filter, includes, ifElse, toLower, values, map, pick, join } from 'rambda';
+import { isNilOrIsEmpty } from '../helpers/rambda-adjunct';
+
+const searchTodo = (q, fields = ['label', 'description']) => filter(
+  compose(
+    includes(toLower(q || '')),
+    join(' '),
+    map(toLower),
+    values,
+    pick(fields)
+  )
+)
 
 export default {
-  name: 'todo-list',
+  name: 'TodList',
   components: {
     TodoListItem,
     ConfirmDialogue,
     TodosCount,
-    TodoFilter
+    TodoFilter,
+    TodoSearch
   },
   data () {
     return {
-      filterMode: 'showOpen'
+      filterMode: 'showOpen',
+      searchData: null
     }
   },
   computed: {
     ...mapGetters(['openTodos', 'allTodos', 'closedTodos']),
     todos () {
+      let todos = [];
       switch (this.filterMode) {
         case 'showAll':
-          return this.allTodos;
+          todos = this.allTodos;
+          break;
         case 'showClosed':
-          return this.closedTodos;
+          todos = this.closedTodos;
+          break;
         default:
-          return this.openTodos;
+          todos = this.openTodos;
       }
+      return ifElse(
+        compose(isNilOrIsEmpty, prop('searchData')),
+        prop('todos'),
+        compose(searchTodo(this.searchData), prop('todos')),
+      )({searchData: this.searchData, todos})
     }
   },
   methods: {
     ...mapActions([
       'removeTodo'
     ]),
+    onSearch (q) {
+      this.searchData = q;
+    },
     async handleRemoveItem (id) {
       const ok = await this.$refs.confirmDialogue.show();
       if (ok) {
@@ -71,7 +98,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
